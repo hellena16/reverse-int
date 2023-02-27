1e9d2df33c78859c4af026e59ff2b151111b7db8
Hellena16 <elena-kuzmitskaya@rambler.ru>
2023-02-27 11:17:12 +0300

    feat: change index,js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+ 
+  if (Math.sign(n) === -1 || Math.sign(n) === -0) {
+    let arr = String(n).split('')
+    arr.splice(0,1)
+    return Number(arr.reverse().join(''))
+  } else {
+    return Number(String(n).split('').reverse().join(''))
+  }
+
 }
+
